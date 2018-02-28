a53207155c07f0f157c8bed1ae351fa537a1286e
minismee3 <2172734P@student.gla.ac.uk>
2018-02-28 17:50:30 +0000

    changed nav bar

--- js/first.js
+++ js/first.js
@@ -1,11 +1,19 @@
-var  nb = $(".navbar");
-    nbs = "navbar-scrolled";
-    hdr = $('header').height();
+$(document).ready(function() {
+  var stickyNavTop = $('.navbar').offset().top;
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(nbs);
-  } else {
-    mn.removeClass(nbs);
-  }
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > stickyNavTop) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+    stickyNav();
+  });
 });
