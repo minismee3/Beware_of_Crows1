54cc2199acb91542389132af735130480a8b9370
minismee3 <2172734P@student.gla.ac.uk>
2018-03-03 14:00:13 +0000

    Fixed carousel
    Now the pictures change when you click the buttons! Also properly
    formatted now, with scrollpsy.

--- js/first.js
+++ js/first.js
@@ -1,19 +1,5 @@
-$(document).ready(function() {
-  var stickyNavTop = $('.navbar').offset().top;
-
-  var stickyNav = function(){
-    var scrollTop = $(window).scrollTop();
-
-    if (scrollTop > stickyNavTop) {
-      $('.navbar').addClass('sticky');
-    } else {
-      $('.navbar').removeClass('sticky');
-    }
-  };
-
-  stickyNav();
-
-  $(window).scroll(function() {
-    stickyNav();
-  });
+$('#myCarousel').each(function() {
+    $(this).carousel({
+        interval: false
+    });
 });
