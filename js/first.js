d0b2b1f9e203cb88999cb15c199cbd3b2ef03009
minismee3 <2172734P@student.gla.ac.uk>
2018-03-04 18:55:53 +0000

    Navbar finished
    Navbar now changing sizes and sticky at top when scrolled. jsut need to
    sort out aesthetics.

--- js/first.js
+++ js/first.js
@@ -3,3 +3,12 @@ $('#myCarousel').each(function() {
         interval: false
     });
 });
+
+$(window).scroll(function (event) {
+            var y = $(this).scrollTop(); //set position from top in pixels
+            if (y >= 675) {
+                $('.navbar').addClass('resized');
+            } else {
+                $('.navbar').removeClass('resized');
+            }
+        });
