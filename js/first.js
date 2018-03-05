f5e4a7ba06b164ea5f61157fa25aab02e332daea
minismee3 <2172734P@student.gla.ac.uk>
2018-03-05 22:41:25 +0000

    Aesthetic Changes
    Changed some colours of the navbar and the background . Put in right
    logo.

--- js/first.js
+++ js/first.js
@@ -1,9 +1,12 @@
+
+<!--carousel changes pics-->
 $('#myCarousel').each(function() {
     $(this).carousel({
         interval: false
     });
 });
 
+<!-- resizes navbar on scroll-->
 $(window).scroll(function (event) {
             var y = $(this).scrollTop(); //set position from top in pixels
             if (y >= 675) {
