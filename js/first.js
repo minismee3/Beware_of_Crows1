4be90a3b7ad155611d47e50c5e07fc47d5c8a82d
minismee3 <2172734P@student.gla.ac.uk>
2018-03-07 18:45:04 +0000

    So beth can get it

--- js/first.js
+++ js/first.js
@@ -7,15 +7,7 @@ $('#myCarousel').each(function() {
 });
 
 //resizes navbar on scroll//
-$(window).scroll(function (event) {
-            var y = $(this).scrollTop(); //set position from top in pixels
-            if (y >= 690) {
-                $('.navbar').addClass('resized');
-            } else {
-                $('.navbar').removeClass('resized');
-            }
-        });
-        $(function(){
+
            // When the toggle areas in your navbar are clicked, toggle them
            $("#search-button, #search-icon").click(function(e){
                e.preventDefault();
