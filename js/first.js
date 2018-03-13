25bac73c354db832638c5eec2eba3789bac6fbe9
minismee3 <2172734P@student.gla.ac.uk>
2018-03-13 13:46:30 +0000

    Gallery
    Sorted it out a bit. Took out search bar too.

--- js/first.js
+++ js/first.js
@@ -23,36 +23,9 @@ $('#myCarousel').each(function() {
                 }
             });
         });
-sss
-        // toggle searchbar//
-           $(function(){
 
-              $("#search-button, #search-icon").click(function(e){
-                  e.preventDefault();
-                  $("#search-button, #search-form").toggle();
-              });
-           })
-         }
-//modal
-// Get the modal
-var modal = document.getElementById('myModal1');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = $('.myImg1');
-var modalImg = $("#img01");
-var captionText = document.getElementById("caption1");
-$('.myImg1').click(function(){
-    modal.style.display = "block";
-    var newSrc = this.src;
-    modalImg.attr('src', newSrc);
-    captionText.innerHTML = this.alt;
-});
-}
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+         $(function(){
+      $("#includedContent").load("/trial.html");
+    });
