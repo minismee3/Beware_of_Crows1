ca4a554b6839109748a7c2f1bbd9d4d14e915400
minismee3 <2172734P@student.gla.ac.uk>
2018-03-15 12:40:16 +0000

    don't like this but have a look

---
+++ README.md
@@ -0,0 +1,2 @@
+Beware of Crows
+Website using html, css and jquery to show off skateboard designs and provide contact info.
--- js/first.js
+++ js/first.js
@@ -23,16 +23,4 @@ $('#myCarousel').each(function() {
                 }
             });
         });
-        $("panel").hover(function(){
-            $(".stuff").toggle();
-        });
-        $('#item-wrapper a').mouseenter(function () {
-            console.log($(this).data('panel'));
-            if ($(this).data('panel')) {
-                $('.panel').hide();
-                $('#' + $(this).data('panel')).show();
-            }
-            });
-        $('#item-wrapper').mouseleave(function () {
-            $('.panel').hide();
-        });
+      
