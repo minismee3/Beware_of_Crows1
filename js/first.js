67ffa565b3e43ab6cc9d7b0d1136d6cfc13736ff
minismee3 <2172734P@student.gla.ac.uk>
2018-03-15 12:20:37 +0000

    judt pushing so i can change

--- js/first.js
+++ js/first.js
@@ -23,9 +23,16 @@ $('#myCarousel').each(function() {
                 }
             });
         });
-
-        $("button3").click(function(){
-            var color = clicked ? 'red' : 'blue';
-            $(this).css('background-color', color);
-            clicked = !clicked;
+        $("panel").hover(function(){
+            $(".stuff").toggle();
+        });
+        $('#item-wrapper a').mouseenter(function () {
+            console.log($(this).data('panel'));
+            if ($(this).data('panel')) {
+                $('.panel').hide();
+                $('#' + $(this).data('panel')).show();
+            }
+            });
+        $('#item-wrapper').mouseleave(function () {
+            $('.panel').hide();
         });
